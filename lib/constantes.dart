41f1d05c65887eec885ab31b2dd3caf5f290a51f
Wenderson Farias <wendersonpfarias@gmail.com>
2024-07-29 03:54:30 -0400

    feat: criar arquivo de constantes para widgets e variáveis globais

---
+++ lib/constantes.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+const kCorAtivaCartaoPadrao = Color(0xFFFF5722);
+
+const kDescricaoTextStyle = TextStyle(
+  fontSize: 20,
+  color: Colors.white,
+  fontWeight: FontWeight.w700,
+  letterSpacing: 0.5,
+  height: 1.5,
+);
