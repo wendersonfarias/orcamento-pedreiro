886ac13d236dfb5c93574e849af8e83afdf69783
Wenderson Farias <wendersonpfarias@gmail.com>
2024-08-01 12:13:10 -0400

    :sparkles: feat: Criação dos widgets para a tela tela_historico.dart

--- lib/telas/tela_historico.dart
+++ lib/telas/tela_historico.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:orcamento_pedreiro/database/db.dart';
+
+import '../modelos/orcamento_modelo.dart';
 
 class TelaHistorico extends StatefulWidget {
   const TelaHistorico({super.key});
@@ -8,12 +11,61 @@ class TelaHistorico extends StatefulWidget {
 }
 
 class _TelaHistoricoState extends State<TelaHistorico> {
+  List<OrcamentoModelo> _orcamentos = [];
+  bool _isLoading = true;
+
+  @override
+  void initState() {
+    super.initState();
+    _carregarOrcamentos();
+  }
+
+  Future<void> _carregarOrcamentos() async {
+    try {
+      DB db = DB.instancia;
+      List<Map<String, dynamic>> orcamentoMaps = await db.getOrcamentos();
+      setState(() {
+        _orcamentos =
+            orcamentoMaps.map((map) => OrcamentoModelo.fromMap(map)).toList();
+        _isLoading = false;
+
+        print(_orcamentos);
+      });
+    } catch (e) {
+      setState(() {
+        _isLoading = false;
+      });
+      print('Erro ao carregar orçamentos: $e');
+      // Adicionar tratamento de erro adequado, como mostrar uma mensagem para o usuário.
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Historico de Orçamentos"),
+        title: Text('Orçamentos Salvos'),
       ),
+      body: _isLoading
+          ? Center(child: CircularProgressIndicator())
+          : ListView.builder(
+              itemCount: _orcamentos.length,
+              itemBuilder: (context, index) {
+                OrcamentoModelo orcamento = _orcamentos[index];
+                return Card(
+                  margin: EdgeInsets.all(10),
+                  child: ListTile(
+                    title: Text('Cliente: ${orcamento.cliente}'),
+                    subtitle: Text('Tipo: ${orcamento.tipoOrcamento}\n'
+                        'Valor: R\$ ${orcamento.valorMaoObra}\n'
+                        'Prazo: ${orcamento.prazoDias} '),
+                    onTap: () {
+                      // Ação ao clicar no Card
+                    },
+                  ),
+                );
+              },
+            ),
     );
   }
 }
