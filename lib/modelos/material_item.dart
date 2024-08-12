67e8c39f76543d86eb21027b0b47676438474ad0
Wenderson Farias <wendersonpfarias@gmail.com>
2024-08-12 03:12:33 -0400

    :sparkles: feat: Criação da classe MaterialItem para representar um item de material

---
+++ lib/modelos/material_item.dart
@@ -0,0 +1,7 @@
+// Classe que representa um item de material
+class MaterialItem {
+  String nomeMaterial;
+  double quantidade;
+
+  MaterialItem({required this.nomeMaterial, required this.quantidade});
+}
