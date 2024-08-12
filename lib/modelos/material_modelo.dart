ce1eff50b01ef28d3b770faab1c066f4c5dc2e4a
Wenderson Farias <wendersonpfarias@gmail.com>
2024-08-12 03:13:37 -0400

    :recycle: refactor: Alteração do campo quantidadeMaterial para quantidade e adição do método converterParaMaterialModelo na classe MaterialModelo

--- lib/modelos/material_modelo.dart
+++ lib/modelos/material_modelo.dart
@@ -1,21 +1,46 @@
+import 'package:orcamento_pedreiro/modelos/material_item.dart';
+
 class MaterialModelo {
   int? idMaterial;
   String nomeMaterial;
-  String quantidadeMaterial;
+  String quantidade;
   int idOrcamento;
+
   MaterialModelo({
     this.idMaterial,
     required this.nomeMaterial,
-    required this.quantidadeMaterial,
+    required this.quantidade,
     required this.idOrcamento,
   });
 
+  // Converte um Map para um objeto MaterialModelo
+  factory MaterialModelo.fromMap(Map<String, dynamic> map) {
+    return MaterialModelo(
+      idMaterial: map['id_material'],
+      nomeMaterial: map['nome_material'],
+      quantidade: map['quantidade'],
+      idOrcamento: map['id_orcamento'],
+    );
+  }
+
+  // Converte um objeto MaterialModelo para um Map
   Map<String, dynamic> toMap() {
     return {
       'id_material': idMaterial,
       'nome_material': nomeMaterial,
-      'quantidade_material': quantidadeMaterial,
+      'quantidade': quantidade,
       'id_orcamento': idOrcamento,
     };
   }
+}
+
+List<MaterialModelo> converterParaMaterialModelo(
+    List<MaterialItem> lista, int idOrcamento) {
+  return lista.map((item) {
+    return MaterialModelo(
+      nomeMaterial: item.nomeMaterial,
+      quantidade: item.quantidade.toString(),
+      idOrcamento: idOrcamento,
+    );
+  }).toList();
 }
