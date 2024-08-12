273e3e33701f4355c93cbabbf7385450f99ff6ea
Wenderson Farias <wendersonpfarias@gmail.com>
2024-08-12 03:17:22 -0400

    :lipstick: feat: Melhoria na exibição na TelaHistorico com formatação de moeda e adição do campo de data

--- lib/telas/tela_historico.dart
+++ lib/telas/tela_historico.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'package:orcamento_pedreiro/database/db.dart';
 
 import '../modelos/orcamento_modelo.dart';
@@ -13,6 +14,9 @@ class TelaHistorico extends StatefulWidget {
 class _TelaHistoricoState extends State<TelaHistorico> {
   List<OrcamentoModelo> _orcamentos = [];
   bool _isLoading = true;
+  bool _hasError = false;
+  NumberFormat real = NumberFormat.currency(locale: 'pt_BR', name: 'R\$');
+  DateFormat dateFormat = DateFormat('dd/MM/yyyy'); // Define o formato de data
 
   @override
   void initState() {
@@ -21,6 +25,11 @@ class _TelaHistoricoState extends State<TelaHistorico> {
   }
 
   Future<void> _carregarOrcamentos() async {
+    setState(() {
+      _isLoading = true;
+      _hasError = false;
+    });
+
     try {
       DB db = DB.instancia;
       List<Map<String, dynamic>> orcamentoMaps = await db.getOrcamentos();
@@ -28,15 +37,13 @@ class _TelaHistoricoState extends State<TelaHistorico> {
         _orcamentos =
             orcamentoMaps.map((map) => OrcamentoModelo.fromMap(map)).toList();
         _isLoading = false;
-
-        print(_orcamentos);
       });
     } catch (e) {
       setState(() {
         _isLoading = false;
+        _hasError = true;
       });
       print('Erro ao carregar orçamentos: $e');
-      // Adicionar tratamento de erro adequado, como mostrar uma mensagem para o usuário.
     }
   }
 
@@ -45,27 +52,64 @@ class _TelaHistoricoState extends State<TelaHistorico> {
     return Scaffold(
       appBar: AppBar(
         title: Text('Orçamentos Salvos'),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.refresh),
+            onPressed: _carregarOrcamentos,
+          ),
+        ],
       ),
       body: _isLoading
           ? Center(child: CircularProgressIndicator())
-          : ListView.builder(
-              itemCount: _orcamentos.length,
-              itemBuilder: (context, index) {
-                OrcamentoModelo orcamento = _orcamentos[index];
-                return Card(
-                  margin: EdgeInsets.all(10),
-                  child: ListTile(
-                    title: Text('Cliente: ${orcamento.cliente}'),
-                    subtitle: Text('Tipo: ${orcamento.tipoOrcamento}\n'
-                        'Valor: R\$ ${orcamento.valorMaoObra}\n'
-                        'Prazo: ${orcamento.prazoDias} '),
-                    onTap: () {
-                      // Ação ao clicar no Card
+          : _hasError
+              ? const Center(
+                  child: Text('Erro ao carregar orçamentos. Tente novamente.'),
+                )
+              : RefreshIndicator(
+                  onRefresh: _carregarOrcamentos,
+                  child: ListView.builder(
+                    itemCount: _orcamentos.length,
+                    itemBuilder: (context, index) {
+                      OrcamentoModelo orcamento = _orcamentos[index];
+                      return Card(
+                        margin: EdgeInsets.all(10),
+                        elevation: 5,
+                        child: ListTile(
+                          contentPadding: EdgeInsets.all(16),
+                          title: Text(
+                            'Cliente: ${orcamento.cliente}',
+                            style: TextStyle(fontWeight: FontWeight.bold),
+                          ),
+                          subtitle: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                'Tipo: ${orcamento.tipoOrcamento.toString().split('.').last}',
+                                style: TextStyle(color: Colors.grey[600]),
+                              ),
+                              Text(
+                                'Valor mão de obra : ${real.format(orcamento.valorMaoObra * double.parse(orcamento.areaOrcada))}',
+                                style: TextStyle(color: Colors.green),
+                              ),
+                              Text(
+                                'Prazo: ${orcamento.prazoDias} dias',
+                                style: TextStyle(color: Colors.blueGrey),
+                              ),
+                              Text(
+                                'Data do orçamento: ${dateFormat.format(orcamento.data)}',
+                                style: TextStyle(color: Colors.blueGrey),
+                              ),
+                            ],
+                          ),
+                          trailing: Icon(Icons.chevron_right),
+                          onTap: () {
+                            // Ação ao clicar no Card
+                          },
+                        ),
+                      );
                     },
                   ),
-                );
-              },
-            ),
+                ),
     );
   }
 }
