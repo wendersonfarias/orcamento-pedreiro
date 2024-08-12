5abf068bae6f5f86310d5ffb626d0d32c858e854
Wenderson Farias <wendersonpfarias@gmail.com>
2024-08-12 03:18:28 -0400

    :sparkles: feat: Criação da classe teste orçamento

---
+++ test/testeOrcamento.dart
@@ -0,0 +1,49 @@
+void main() {
+  /*group('OrcamentoModelo', () {
+    test('deve converter de Map para OrcamentoModelo corretamente', () {
+      // Dados de exemplo
+      final map = {
+        'id_orcamento': 1,
+        'tipo_orcamento': 'muro',
+        'cliente': 'João Silva',
+        'data': '2024-08-11',
+        'valor_mao_obra': 1500.0,
+        'prazo_dias': '30',
+      };
+
+      // Converta o Map para OrcamentoModelo
+      final orcamento = OrcamentoModelo.fromMap(map);
+
+      // Verifique se o objeto OrcamentoModelo está correto
+      expect(orcamento.idOrcamento, 1);
+      expect(orcamento.tipoOrcamento, TipoOrcamento.muro);
+      expect(orcamento.cliente, 'João Silva');
+      expect(orcamento.data, '2024-08-11');
+      expect(orcamento.valorMaoObra, 1500.0);
+      expect(orcamento.prazoDias, '30');
+    });
+
+    test('deve converter de OrcamentoModelo para Map corretamente', () {
+      // Dados de exemplo
+      final orcamento = OrcamentoModelo(
+        idOrcamento: 1,
+        tipoOrcamento: TipoOrcamento.ceramica,
+        cliente: 'Maria Oliveira',
+        data: '2024-08-12',
+        valorMaoObra: 2000.0,
+        prazoDias: '45',
+      );
+
+      // Converta o OrcamentoModelo para Map
+      final map = orcamento.toMap();
+
+      // Verifique se o Map está correto
+      expect(map['id_orcamento'], 1);
+      expect(map['tipo_orcamento'], 'ceramica');
+      expect(map['cliente'], 'Maria Oliveira');
+      expect(map['data'], '2024-08-12');
+      expect(map['valor_mao_obra'], 2000.0);
+      expect(map['prazo_dias'], '45');
+    });
+  });*/
+}
