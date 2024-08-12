86333239c95269bcc0d6bb022c90b4f9853a7584
Wenderson Farias <wendersonpfarias@gmail.com>
2024-08-12 03:11:10 -0400

    :sparkles: feat: Atualização do script de criação das tabelas e adição de novos métodos na classe db.dart

--- lib/database/db.dart
+++ lib/database/db.dart
@@ -1,3 +1,5 @@
+import 'package:orcamento_pedreiro/modelos/material_modelo.dart';
+import 'package:orcamento_pedreiro/modelos/orcamento_modelo.dart';
 import 'package:path/path.dart';
 import 'package:sqflite/sqflite.dart';
 
@@ -20,7 +22,7 @@ class DB {
 
   Future<Database> _initDatabase() async {
     return openDatabase(
-      join(await getDatabasesPath(), 'banco_orcamentos.db'),
+      join(await getDatabasesPath(), 'orçamento10.db'),
       version: 1,
       onCreate: _onCreate,
     );
@@ -34,21 +36,21 @@ class DB {
   String get _orcamento => '''
     CREATE TABLE orcamento (
     id_orcamento INTEGER PRIMARY KEY AUTOINCREMENT,
-    tipo_orcamento TEXT,
-    cliente TEXT,
-    data TEXT,
-    valor_mao_obra REAL,
-    prazo_dias TEXT,
-    status_orcamento TEXT );
+    tipo_orcamento TEXT NOT NULL,
+    cliente TEXT NOT NULL,
+    data INTEGER NOT NULL,
+    valor_mao_obra REAL NOT NULL,
+    prazo_dias TEXT NOT NULL,
+    area_orcada TEXT NOT NULL );
   ''';
 
   String get _material => '''
     CREATE TABLE Material (
-    id_material INTEGER PRIMARY KEY AUTOINCREMENT,
-    nome_material TEXT,
-    quantidade_material TEXT,
-    id_orcamento INTEGER,
-    FOREIGN KEY (id_orcamento) REFERENCES orcamento(id_orcamento) );
+      id_material INTEGER PRIMARY KEY AUTOINCREMENT,
+      nome_material TEXT NOT NULL,
+      quantidade TEXT NOT NULL,
+      id_orcamento INTEGER NOT NULL,
+      FOREIGN KEY (id_orcamento) REFERENCES orcamento(id_orcamento));
   ''';
 
   // CRUD para a tabela orcamento
@@ -59,10 +61,33 @@ class DB {
     return await db.insert('orcamento', orcamento);
   }
 
+  Future<int> inserirOrcamento(OrcamentoModelo orcamento) async {
+    Database db = await database;
+    return await db.insert(
+      'orcamento',
+      orcamento.toMap(),
+      conflictAlgorithm: ConflictAlgorithm.replace,
+    );
+  }
+
+  DateTime getDateFromTimestamp(int timestamp) {
+    return DateTime.fromMillisecondsSinceEpoch(timestamp);
+  }
+
   // Read all
   Future<List<Map<String, dynamic>>> getOrcamentos() async {
     Database db = await database;
     return await db.query('orcamento');
+  }
+
+  // Read all
+  Future<List<OrcamentoModelo>> buscarOrcamentos() async {
+    Database db = await database;
+    final List<Map<String, dynamic>> maps = await db.query('orcamento');
+
+    return List.generate(maps.length, (i) {
+      return OrcamentoModelo.fromMap(maps[i]);
+    });
   }
 
   // Read one
@@ -77,6 +102,21 @@ class DB {
       return results.first;
     }
     return null;
+  }
+
+  Future<OrcamentoModelo?> buscarOrcamentoPorId(int id) async {
+    final db = await database;
+    final List<Map<String, dynamic>> maps = await db.query(
+      'orcamento',
+      where: 'id_orcamento = ?',
+      whereArgs: [id],
+    );
+
+    if (maps.isNotEmpty) {
+      return OrcamentoModelo.fromMap(maps.first);
+    } else {
+      return null; // Retorna null se o orçamento não for encontrado
+    }
   }
 
   // Update
@@ -102,14 +142,51 @@ class DB {
 
   // CRUD para a tabela material
 
-  Future<int> insertMaterial(Map<String, dynamic> material) async {
+  // Future<int> insertMaterial(Map<String, dynamic> material) async {
+  //   Database db = await database;
+  //   return await db.insert(
+  //     'material',
+  //     material.toMap(),
+  //     conflictAlgorithm: ConflictAlgorithm.replace,
+  //   );
+  // }
+
+  Future<int> inserirMaterial(MaterialModelo material) async {
     Database db = await database;
-    return await db.insert('material', material);
+    return await db.insert(
+      'material',
+      material.toMap(),
+      conflictAlgorithm: ConflictAlgorithm.replace,
+    );
+  }
+
+  Future<void> inserirListaMateriais(List<MaterialModelo> materiais) async {
+    final db = await database;
+    Batch batch = db.batch();
+
+    for (var material in materiais) {
+      batch.insert(
+        'material',
+        material.toMap(),
+        conflictAlgorithm: ConflictAlgorithm.replace,
+      );
+    }
+
+    await batch.commit();
   }
 
   Future<List<Map<String, dynamic>>> getMateriais() async {
     Database db = await database;
     return await db.query('material');
+  }
+
+  Future<List<MaterialModelo>> buscarMateriais() async {
+    final db = await database;
+    final List<Map<String, dynamic>> maps = await db.query('material');
+
+    return List.generate(maps.length, (i) {
+      return MaterialModelo.fromMap(maps[i]);
+    });
   }
 
   Future<Map<String, dynamic>?> getMaterial(int id) async {
@@ -133,6 +210,21 @@ class DB {
       where: 'id_orcamento = ?',
       whereArgs: [orcamentoId],
     );
+  }
+
+  // Método para buscar materiais pelo ID do orçamento
+  Future<List<MaterialModelo>> buscarMateriaisOrcamentoId(
+      int idOrcamento) async {
+    final db = await database;
+    final List<Map<String, dynamic>> maps = await db.query(
+      'material',
+      where: 'id_orcamento = ?',
+      whereArgs: [idOrcamento],
+    );
+
+    return List.generate(maps.length, (i) {
+      return MaterialModelo.fromMap(maps[i]);
+    });
   }
 
   Future<int> updateMaterial(Map<String, dynamic> material) async {
