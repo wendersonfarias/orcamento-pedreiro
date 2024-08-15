9e93d3bc6f39b0fd95b584ee14484e16170f59c3
Wenderson Farias <wendersonpfarias@gmail.com>
2024-08-14 22:56:37 -0400

    :recycle: refactor: Remoção do widget Expanded na tela TelaOrcamento

--- lib/telas/tela_orcamento.dart
+++ lib/telas/tela_orcamento.dart
@@ -20,89 +20,83 @@ class TelaOrcamento extends StatelessWidget {
       ),
       body: ListView(
         children: <Widget>[
-          Expanded(
-            child: Row(
-              children: [
-                Expanded(
-                  child: CartaoPadrao(
-                    aoPressionar: () {
-                      Navigator.pushNamed(context, '/orcamento-muro');
-                    },
-                    filhoCartao: ConteudoIcone(
-                      nomeImagem: "tijolo",
-                      descricao: "Muro",
-                    ),
+          Row(
+            children: [
+              Expanded(
+                child: CartaoPadrao(
+                  aoPressionar: () {
+                    Navigator.pushNamed(context, '/orcamento-muro');
+                  },
+                  filhoCartao: ConteudoIcone(
+                    nomeImagem: "tijolo",
+                    descricao: "Muro",
                   ),
                 ),
-                Expanded(
-                  child: CartaoPadrao(
-                    filhoCartao: ConteudoIcone(
-                      nomeImagem: "piso",
-                      descricao: "Contrapiso",
-                    ),
-                    aoPressionar: () {
-                      Navigator.pushNamed(context, '/orcamento-contrapiso');
-                    },
+              ),
+              Expanded(
+                child: CartaoPadrao(
+                  filhoCartao: ConteudoIcone(
+                    nomeImagem: "piso",
+                    descricao: "Contrapiso",
                   ),
-                )
-              ],
-            ),
+                  aoPressionar: () {
+                    Navigator.pushNamed(context, '/orcamento-contrapiso');
+                  },
+                ),
+              )
+            ],
           ),
-          Expanded(
-            child: Row(
-              children: [
-                Expanded(
-                  child: CartaoPadrao(
-                    filhoCartao: ConteudoIcone(
-                      nomeImagem: "teto",
-                      descricao: "Forro PVC",
-                    ),
-                    aoPressionar: () {
-                      Navigator.pushNamed(context, '/orcamento-forro');
-                    },
+          Row(
+            children: [
+              Expanded(
+                child: CartaoPadrao(
+                  filhoCartao: ConteudoIcone(
+                    nomeImagem: "teto",
+                    descricao: "Forro PVC",
                   ),
+                  aoPressionar: () {
+                    Navigator.pushNamed(context, '/orcamento-forro');
+                  },
                 ),
-                Expanded(
-                  child: CartaoPadrao(
-                    filhoCartao: ConteudoIcone(
-                      nomeImagem: "calcada",
-                      descricao: "Calçada",
-                    ),
-                    aoPressionar: () {
-                      Navigator.pushNamed(context, '/orcamento-calcada');
-                    },
+              ),
+              Expanded(
+                child: CartaoPadrao(
+                  filhoCartao: ConteudoIcone(
+                    nomeImagem: "calcada",
+                    descricao: "Calçada",
                   ),
-                )
-              ],
-            ),
+                  aoPressionar: () {
+                    Navigator.pushNamed(context, '/orcamento-calcada');
+                  },
+                ),
+              )
+            ],
           ),
-          Expanded(
-            child: Row(
-              children: [
-                Expanded(
-                  child: CartaoPadrao(
-                    filhoCartao: ConteudoIcone(
-                      nomeImagem: "ceramica",
-                      descricao: "Cerâmica",
-                    ),
-                    aoPressionar: () {
-                      Navigator.pushNamed(context, '/orcamento-ceramica');
-                    },
+          Row(
+            children: [
+              Expanded(
+                child: CartaoPadrao(
+                  filhoCartao: ConteudoIcone(
+                    nomeImagem: "ceramica",
+                    descricao: "Cerâmica",
                   ),
+                  aoPressionar: () {
+                    Navigator.pushNamed(context, '/orcamento-ceramica');
+                  },
                 ),
-                Expanded(
-                  child: CartaoPadrao(
-                    filhoCartao: ConteudoIcone(
-                      nomeImagem: "cobertura",
-                      descricao: "Telhado",
-                    ),
-                    aoPressionar: () {
-                      Navigator.pushNamed(context, '/orcamento-telhado');
-                    },
+              ),
+              Expanded(
+                child: CartaoPadrao(
+                  filhoCartao: ConteudoIcone(
+                    nomeImagem: "cobertura",
+                    descricao: "Telhado",
                   ),
-                )
-              ],
-            ),
+                  aoPressionar: () {
+                    Navigator.pushNamed(context, '/orcamento-telhado');
+                  },
+                ),
+              )
+            ],
           ),
         ],
       ),
